4fdc3fafa1af2c388a12ef983266f8f86f2b140c
BAOFU\ruifeng_song <>
2019-03-26 19:52:46 +0800

    移动端/pc   活动主页  售卖课程（腾讯课堂）

---
+++ ku.js
@@ -0,0 +1,51 @@
+function loadTxt(paramList){
+		var valueArray=[];
+		for(var i=0;i<paramList.length;i++){
+			valueArray.push(paramList[i]);
+		}
+		return valueArray;
+}
+
+//金融
+jingrong=[
+	{"imgleftsrc":"imgs/j1.jpg","title":"2017年6月CFA二+三级特惠取证班","titleA":"【2017年6月开班，预报从速】","titleB":"二三级学练测相结合，通过率更放心！","price":"17800","priceold":"￥17800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/j11.jpg"},
+	{"imgleftsrc":"imgs/j2.jpg","title":"CFA一+二+三级特惠取证班","titleA":"【2017年6月开班，预报从速】","titleB":"三级连报更优惠，省心放心！","price":"23800","priceold":"￥32800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/j12.jpg"},
+	{"imgleftsrc":"imgs/j3.jpg","title":"2017年5月FRM一级零基础长线班","titleA":"【2017年6月开班，预报从速】","titleB":"FRM一级，零基础也不用愁！","price":"9580","priceold":"￥12880","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/j13.jpg"},
+	{"imgleftsrc":"imgs/j4.jpg","title":"2017年5月FRM一+二级特惠取证班","titleA":"【2017年6月开班，预报从速】","titleB":"FRM一二级连报，省心放心！","price":"15800","priceold":"￥20760","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/j14.jpg"},
+	{"imgleftsrc":"imgs/j5.jpg","title":"2017年5月FRM一+二级无忧取证班","titleA":"【2017年6月开班，预报从速】","titleB":"FRM一二级连报，省心放心！","price":"15800","priceold":"￥20760","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/j14.jpg"}
+];
+
+//从业
+congye=[
+	{"imgleftsrc":"imgs/c1.jpg","title":"证券市场基本法律法规","titleA":"【High新年特惠，预报从速】","titleB":"证券从业","price":"299","priceold":"￥998","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/c11.jpg"},
+	{"imgleftsrc":"imgs/c2.jpg","title":"金融市场基础知识","titleA":"【High新年特惠，预报从速】","titleB":"证券从业","price":"299","priceold":"￥998","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/c11.jpg"},
+	{"imgleftsrc":"imgs/c3.jpg","title":"证券投资基金基础知识","titleA":"【High新年特惠，预报从速】","titleB":"基金从业","price":"299","priceold":"￥998","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/c11.jpg"},
+	{"imgleftsrc":"imgs/c4.jpg","title":"基金法律法规、职业道德与业务规范","titleA":"【High新年特惠，预报从速】","titleB":"基金从业","price":"299","priceold":"￥998","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/c11.jpg"},
+	{"imgleftsrc":"imgs/c5.jpg","title":"会计电算化","titleA":"【High新年特惠，预报从速】","titleB":"会计从业","price":"299","priceold":"￥998","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/c11.jpg"},
+	{"imgleftsrc":"imgs/c6.jpg","title":"会计基础","titleA":"【High新年特惠，预报从速】","titleB":"会计从业","price":"299","priceold":"￥998","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/c11.jpg"},
+	{"imgleftsrc":"imgs/c7.jpg","title":"法规与道德","titleA":"【High新年特惠，预报从速】","titleB":"会计从业","price":"299","priceold":"￥998","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/c11.jpg"}
+];
+
+//财会
+caihui=[
+	{"imgleftsrc":"imgs/ca1.jpg","title":"ACCAF1-F3冲刺模考题","titleA":"【High新年特惠，预报从速】","titleB":"F阶段1-3级冲刺模考题","price":"128","priceold":"￥399","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca111.jpg"},
+	{"imgleftsrc":"imgs/ca2.jpg","title":"CPA2016网络大师班","titleA":"【名师指导，预报从速】","titleB":"名师授课，学霸必报","price":"6400","priceold":"￥12300","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca112.jpg"},
+	{"imgleftsrc":"imgs/ca3.jpg","title":"老汤煲会计系列二：合并财务报表","titleA":"【名师指导，预报从速】","titleB":"老汤煲会计系列课程","price":"199","priceold":"￥399","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca111.jpg"},
+	{"imgleftsrc":"imgs/ca4.jpg","title":"老汤煲会计系列一：长期股权投资","titleA":"【名师指导，预报从速】","titleB":"老汤煲会计系列课程","price":"149","priceold":"￥399","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca111.jpg"},
+	{"imgleftsrc":"imgs/ca5.jpg","title":"CPA必背系列：基础必背39题","titleA":"【名师指导，预报从速】","titleB":"基础39题，包学包会","price":"195","priceold":"￥195","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca111.jpg"},
+	{"imgleftsrc":"imgs/ca6.jpg","title":"CPA无忧定制班&nbsp;会计","titleA":"【名师指导，预报从速】","titleB":"专属定制班，过线刷分无忧","price":"1600","priceold":"￥1800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"},
+	{"imgleftsrc":"imgs/ca7.jpg","title":"CPA无忧定制班&nbsp;财管","titleA":"【名师指导，预报从速】","titleB":"专属定制班，过线刷分无忧","price":"1600","priceold":"￥1800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"},
+	{"imgleftsrc":"imgs/ca8.jpg","title":"CPA无忧定制班&nbsp;审计","titleA":"【名师指导，预报从速】","titleB":"专属定制班，过线刷分无忧","price":"1600","priceold":"￥1800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"},
+	{"imgleftsrc":"imgs/ca9.jpg","title":"CPA无忧定制班&nbsp;战略","titleA":"【名师指导，预报从速】","titleB":"专属定制班，过线刷分无忧","price":"1600","priceold":"￥1800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"},
+	{"imgleftsrc":"imgs/ca10.jpg","title":"CPA无忧定制班&nbsp;税法","titleA":"【名师指导，预报从速】","titleB":"专属定制班，过线刷分无忧","price":"1600","priceold":"￥1800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"},
+	{"imgleftsrc":"imgs/ca11.jpg","title":"CPA无忧定制班&nbsp;经济法","titleA":"【名师指导，预报从速】","titleB":"专属定制班，过线刷分无忧","price":"1600","priceold":"￥1800","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"},
+	{"imgleftsrc":"imgs/ca12.jpg","title":"ChFP三级精品无忧班","titleA":"【2016年前大促，预报从速】","titleB":"名师授课，考试无忧","price":"3200","priceold":"￥3500","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"},
+	{"imgleftsrc":"imgs/ca13.jpg","title":"ChFP二级精品无忧班","titleA":"【2016年前大促，预报从速】","titleB":"名师授课，考试无忧","price":"4200","priceold":"￥4500","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/ca118.jpg"}
+];
+
+//中级经济师
+zhongjijingjishi=[
+	{"imgleftsrc":"imgs/z1.jpg","title":"经济基础知识","titleA":"【High新年特惠，预报从速】","titleB":"学经济找金程，名师手把手教学","price":"680","priceold":"￥880","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/z11.jpg"},
+	{"imgleftsrc":"imgs/z2.jpg","title":"金融专业知识与实务","titleA":"【High新年特惠，预报从速】","titleB":"学经济找金程，名师手把手教学","price":"680","priceold":"￥880","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/z11.jpg"},
+	{"imgleftsrc":"imgs/z3.jpg","title":"中级经济师精品通关班（经济+金融）","titleA":"【High新年特惠，预报从速】","titleB":"名师教学，学霸必选","price":"1200","priceold":"￥1400","detail":"用户可在2016年12月25日0:00至2016年12月31日23:59期间内购买预定券。在预定券有效期内购买课程即可抵用。","imgRightSrc":"imgs/z11.jpg"}
+];
